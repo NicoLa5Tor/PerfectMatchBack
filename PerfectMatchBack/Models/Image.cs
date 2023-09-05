f32efa4f49e6ecc087734036649a7a3803ea863e
LAPTOP-LOLREGVU\Nicolas Torres <nico.rodriguez.torres.71@gmail.com>
2023-09-04 20:32:52 -0500

    de imagen byte a string

--- PerfectMatchBack/Models/Image.cs
+++ PerfectMatchBack/Models/Image.cs
@@ -9,7 +9,7 @@ public partial class Image
 
     public int IdPublication { get; set; }
 
-    public byte[] DataImage { get; set; } = null!;
+    public string DataImage { get; set; } = null!;
 
     public virtual Publication IdPublicationNavigation { get; set; } = null!;
 }

76c8aa8207e02fb3627dc276db2acb59ae7a2c4b
David Contreras <drcecontre@gmail.com>
2023-09-27 18:24:06 -0500

    Add Controllers and fixed bugs

---
+++ PerfectMatchBack/Controllers/AnimalTypeController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PerfectMatchBack.DTOs;
+using PerfectMatchBack.Services.Contract;
+
+namespace PerfectMatchBack.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AnimalTypeController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IAnimalTypeService _service;
+        public AnimalTypeController(IMapper mapper,IAnimalTypeService service)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet("List")]
+        public async Task<IActionResult> ListAnimalType()
+        {
+            var list = await _service.ListAnimalType();
+                var listDTO = _mapper.Map<List<AnimalTypeDTO>>(list);
+            if(listDTO.Count > 0)
+            {
+                return Ok(listDTO);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ PerfectMatchBack/Controllers/GenderController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PerfectMatchBack.DTOs;
+using PerfectMatchBack.Models;
+using PerfectMatchBack.Services.Contract;
+
+namespace PerfectMatchBack.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenderController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IGenderService _service;
+        public GenderController(IMapper mapper,IGenderService service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+        [HttpGet("List")]
+        public async Task<IActionResult> ListGenders()
+        {
+            var list = await _service.ListGender();
+            var listDTO = _mapper.Map<List<GenderDTO>>(list);
+            if (listDTO.Count > 0)
+            {
+                return Ok(listDTO);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}

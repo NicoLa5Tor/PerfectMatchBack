04e5caa86ee24c8bdad87b865c16d960e68f9aa0
NicoLa5Tor <nico.rodriguez.torres.71@gmail.com>
2023-09-28 15:53:24 -0500

    pruebas incorpordas

---
+++ TestingBackPM/TestCity.cs
@@ -0,0 +1,30 @@
+﻿using PerfectMatchBack.Models;
+using PerfectMatchBack.Services.Contract;
+using PerfectMatchBack.Services.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingBackPM
+{
+    public class TestCity
+    {
+        private readonly PerfectMatchContext _context;
+        private readonly ICityService _service;
+
+        public TestCity()
+        {
+            _context = new PerfectMatchContext();
+            _service = new CityService(_context);
+        }
+        [Fact]
+        public async Task GetListCity()
+        {
+            var result = await _service.ListCity();
+            var resultCity = Assert.IsType<List<City>>(result);
+            Assert.True(resultCity.Count > 0);
+        }
+    }
+}
---
+++ TestingBackPM/TestGender.cs
@@ -0,0 +1,29 @@
+﻿using PerfectMatchBack.Models;
+using PerfectMatchBack.Services.Contract;
+using PerfectMatchBack.Services.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingBackPM
+{
+    public class TestGender
+    {
+        private readonly PerfectMatchContext _context;
+        private readonly IGenderService _service;
+        public TestGender()
+        {
+            _context = new PerfectMatchContext();
+            _service = new GenderService(_context);
+        }
+        [Fact]
+        public async Task GetListGender()
+        {
+            var result = await _service.ListGender();
+            var resultGender = Assert.IsType<List<Gender>>(result);
+            Assert.True(resultGender.Count>0);
+        }
+    }
+}
